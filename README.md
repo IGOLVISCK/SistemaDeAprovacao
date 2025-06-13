be0b8c5dd14a1986003219586d46c07e7777a7ce
IGOR DA SILVA REIS <igorsreis2017@gmail.com>
2025-06-13 00:35:23 -0300

    Alerts, Alunos List, Resultados

---
+++ README.md
@@ -0,0 +1,6 @@
+# SistemaDeAprovacao
+Se você busca um sistema de aprovação para seus alunos, onde é possível determinar de forma precisa se foram aprovados ou reprovados, você encontrou o lugar certo!
+
+Com nossa ferramenta, você pode inserir as notas de todos os bimestres, definir o limite de faltas permitidas e estabelecer a média mínima de aprovação com total flexibilidade.
+
+Utilize este sistema para otimizar a gestão acadêmica e garantir a clareza no processo de avaliação. Aproveite ao máximo todos os recursos!
---
+++ app.js
@@ -0,0 +1,101 @@
+function botaoResultado() {
+    validarName();
+    validarFaltas();
+    validarMedia();
+    
+
+    const name = document.getElementById("inputname");
+    const maxFaltas = document.getElementById('maxFaltas');
+    const mediaMin = document.getElementById('mediaMin');
+    const inputF = document.getElementById('inputFalta');
+    const inputN = document.querySelectorAll('.inputNota');
+    const resultado = document.getElementById('resultado');
+    
+    let soma = 0;
+
+    inputN.forEach(input => {  //A função forEach() percorre cada input da lista.
+        soma += (Number(input.value)) || 0; //Number(input.value) converte oq esta no input para numero e logo após / por 4
+    }); // || 0 é uma proteção: se o valor for vazio ("") ou inválido, ele usa 0 (evita que NaN entre na conta).
+
+    let media = soma / 4;
+
+    if (media >= (mediaMin.value) && (inputF.value) <= (maxFaltas.value)) {
+        setAprovado('APROVADO');
+        // document.getElementById('resultado').innerHTML = `O aluno ${name.value} foi APROVADO`
+    } else {
+        setReprovado('REPROVADO');
+    }
+
+    const alunos = [];
+    function criaAluno(nome, media) {
+        return { nome, media };
+    }
+    const aluno = criaAluno(name.value, Number(media));
+    alunos.push(aluno)
+    console.log(alunos)
+
+    function setAprovado(msg) {
+        const resultado = document.getElementById('resultado');
+        const paragrafo = document.createElement('p');
+        paragrafo.classList.add('paragrafo-resultadoAprovado');
+        paragrafo.innerHTML = `Aluno ${name.value} ${msg} com ${media.toFixed(2)} de média e ${inputF.value} faltas`;
+        resultado.appendChild(paragrafo);
+        console.log(paragrafo)
+        return (paragrafo);
+    }
+    function setReprovado(msg) {
+
+        const paragrafo = document.createElement('p');
+        paragrafo.classList.add('paragrafo-resultadoReprovado');
+        paragrafo.innerHTML = `Aluno ${name.value} ${msg} com ${media.toFixed(2)} de média e ${inputF.value} faltas`;
+        resultado.appendChild(paragrafo);
+        console.log(paragrafo)
+        return (paragrafo);
+    }
+
+    limparCampos();
+}
+
+
+function validarName() {
+    const validaName = document.getElementById("inputname");
+
+    if (validaName.value.trim() === '') {
+        alert("Digite o nome do aluno (-_-)");
+        paragrafo.innerHTML = ''
+        return;
+    }
+}
+function validarFaltas() {
+    const validaFaltas = document.getElementById("maxFaltas");
+
+    if (validaFaltas.value.trim() === '') {
+        alert("Digite um número máximo de faltas permitidas xD");
+        paragrafo.innerHTML = ''
+        return;
+    }
+}
+function validarMedia() {
+    const validaMedia = document.getElementById("mediaMin");
+
+    if (validaMedia.value.trim() === '') {
+        alert("Digite digite uma média miníma para aprovação :)");
+        paragrafo.innerHTML = ''
+        return;
+    }
+}
+
+
+
+function limparCampos() {
+    const inputs = document.querySelectorAll('input');
+
+    inputs.forEach(input => {
+        input.value = ''
+    });
+}
+
+function limparAlunos() {
+    document.getElementById('resultado').textContent = '...'
+    alert('Tem certeza que deseja limpar a lista de alunos?')
+}
